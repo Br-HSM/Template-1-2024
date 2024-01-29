f01f924ff0863826407f73c4f0fae874292ac823
BrHoussem <brhhsm55@gmail.com>
2024-01-29 17:27:27 +0100

    First commit

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+
+function BarClick() {
+    let UL = document.getElementById("links");
+    if (UL.style.display === "none") {
+        UL.style.display = "block";
+    } else {
+        UL.style.display = "none";
+    }
+    }
